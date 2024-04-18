416d293fbb8426e48111d1252a993609abee78ac
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 12:32:15 -0500

    feat: add core concepts

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 // Import image as object and automaticcally make it's value the path when referenced in JSX
 import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,12 +23,39 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentImg}
+            />
+            <CoreConcept 
+            title="Props"
+            description="props stuff"
+            image={componentImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
