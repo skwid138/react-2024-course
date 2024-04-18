b8b1f3fd4c8adea257c2e9fd910239880e5da0f0
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 17:26:34 -0500

    refactor: move components into their own files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,48 +1,6 @@
-// Import image as object and automaticcally make it's value the path when referenced in JSX
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
-// destructre props object for more readable shorter code
-function CoreConceptDestructure({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from './components/Header.jsx'
+import { CoreConcept, CoreConceptDestructure } from './components/CoreConcept.jsx'
 
 function App() {
   return (
---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,20 @@
+export function CoreConcept(props) {
+    return (
+      <li>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    );
+  }
+  
+  // destructre props object for more readable shorter code
+  export function CoreConceptDestructure({image, title, description}) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
