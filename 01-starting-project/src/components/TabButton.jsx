99558f0813d31434d3c5fcc872f144fc5ae9c0a1
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 17:59:44 -0500

    feat: add button handler

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,6 +1,12 @@
 export default function (props) {
+    function clickHandler() {
+        console.log('suh dude');
+    }
+
     // props.children is the value between a component opening and closing tags
     return (
-        <li><button>{props.children}</button></li>
+        <li>
+            <button onClick={clickHandler}>{props.children}</button>
+        </li>
     );
 }
