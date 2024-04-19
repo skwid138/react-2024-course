ceffe4cded589342aacfaa5c8a1ba524897534a3
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 17:19:30 -0500

    refactor: use turns to derive active player

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,19 +3,26 @@ import Player from './components/Player/Player';
 import GameBoard from './components/GameBoard/GameBoard';
 import Log from './components/Log/Log';
 
+function deriveActivePlayer(gameTurns) {
+  // Rather than use a different piece of state, using this will ensure it's the latest and most accurate data
+  let currPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currPlayer = 'O';
+  }
+
+  return currPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function selectSquareHandler(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+    //setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
 
     setGameTurns((prevTurns) => {
-      // Rather than use a different piece of state, using this will ensure it's the latest and most accurate data
-      let currPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currPlayer = 'O';
-      }
+      const currPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currPlayer },
         ...prevTurns
