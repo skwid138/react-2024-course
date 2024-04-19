d11a56273ea0585af1aaa4f26bd4b03f4f5282b9
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 15:05:04 -0500

    feat: make button ntext dynamic

--- 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -3,18 +3,25 @@ import { useState } from 'react';
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  function editHandler () {
-    setIsEditing(true);
+  function editHandler() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let buttonText = 'Edit';
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    buttonText = 'Save';
   }
 
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input />}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={editHandler}>Edit</button>
+      <button onClick={editHandler}>{buttonText}</button>
     </li>
   );
 }
