977f26c111cb6185c4a0c27f05b4bcf5efb222ea
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 19:59:12 -0500

    refactor: loop concepts

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -22,14 +22,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConceptDestructure {...CORE_CONCEPTS[2]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            {CORE_CONCEPTS.map((conceptObj) => (
+              <CoreConcept key={conceptObj.title} {...conceptObj} />
+            ))}
           </ul>
         </section>
         <section id="examples">
