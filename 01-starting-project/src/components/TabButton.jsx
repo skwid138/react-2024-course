7f53ac2b3dfb1a930c06307e152313290a700303
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 19:58:36 -0500

    fix: prevent bool warning

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -2,7 +2,7 @@ export default function ({ children, onSelect, isSelected }) {
     // props.children is the value between a component opening and closing tags
     return (
         <li>
-            <button className={isSelected && 'active'} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
         </li>
     );
 }
