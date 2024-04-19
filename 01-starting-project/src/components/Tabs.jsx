121942bb19eb9193bd48a04dfb114e63eadcd5cd
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 11:40:27 -0500

    refactor: set tabs component dynamically

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,13 +1,14 @@
 // props.children is the value between a component opening and closing tags
 // Use JS 'Rest property' to catch all undeclared arguments
-export default function ({ buttons, children, ...attributes }) {
+export default function ({ buttonContainer, buttons, children, ...attributes }) {
+  const ButtonContainer = buttonContainer;
   return (
     // Spread all undeclared args, this is helpful for whatever attributes you may add anywhere this is used
     <>
-      <menu {...attributes}>
-        {buttons}
-      </menu>
-      {children}
+    <ButtonContainer {...attributes}>
+      {buttons}
+    </ButtonContainer>
+    {children}
     </>
   );
 }
