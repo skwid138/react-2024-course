03da6db9679dbd741f0d11c180cef7f29ca63807
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-18 19:22:15 -0500

    feat: display example data

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,6 @@
 // All React functions that have a 'use' preface are called hooks
 import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import { CoreConcept, CoreConceptDestructure } from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
@@ -40,7 +40,15 @@ function App() {
             <TabButton onSelect={() => selectHandler('props')}>Props</TabButton>
             <TabButton onSelect={() => selectHandler('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic] ? EXAMPLES[selectedTopic].title : selectedTopic}</h3>
+            <p>{EXAMPLES[selectedTopic] ? EXAMPLES[selectedTopic].description : ''}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic] ? EXAMPLES[selectedTopic].code : ''}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
