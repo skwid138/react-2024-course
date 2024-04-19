6abfb0d404a6f699e93812cb6f7764e26e4a5daf
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 15:12:33 -0500

    fix: use recomended state update process

--- 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -4,7 +4,13 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function editHandler() {
-    setIsEditing(!isEditing);
+    // The following is considered poor practice and advised against by the react devs
+    // Setting state based off current state should not be done
+    // The recomended way would be with a function instead
+    //setIsEditing(!isEditing);
+
+    // The first argument is the current state and the returned value will be the updated state
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
