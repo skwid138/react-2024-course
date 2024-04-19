221d0d1fe605139ad147fc707cdfdabb8894fa02
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 17:10:53 -0500

    feat: output logs

--- 07-tic-tac-toe-starting-project/src/components/Log/Log.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Log/Log.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
 import './Log.css';
 
-export default function Log() {
+export default function Log({ turns }) {
   return (
-  <ol id="log">
-    
-  </ol>
+    <ol id="log">
+      {turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>
+        {turn.player} selected {turn.square.row}, {turn.square.col}
+      </li>)}
+    </ol>
   );
 }
