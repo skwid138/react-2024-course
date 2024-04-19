b8dc60db716575f26cf1c6265ee8a79b957a4898
Hunter Rancourt <hunter.rancourt@gmail.com>
2024-04-19 14:56:17 -0500

    feat: add edit handler

--- 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function editHandler () {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input />}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editHandler}>Edit</button>
     </li>
   );
 }
